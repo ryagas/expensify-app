5ce7e1f457fb20972a4679235afd31256296f391
Ryan C <me@ryancampbell.name>
2019-10-01 18:17:32 -0700

    Typo fix

--- src/tests/components/ExpenseDashboardPage.test.js
+++ src/tests/components/ExpenseDashboardPage.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import ExpenseDashboardPage from '../../components/ExpenseDashBoardPage';
+import ExpenseDashboardPage from '../../components/ExpenseDashboardPage';
 
 test('should render dashboard page', () => {
   const wrapper = shallow(<ExpenseDashboardPage />);
