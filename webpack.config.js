91ec0eec1c44158823617cbfa62fb220f7e37307
Ryan C <me@ryancampbell.name>
2019-10-03 18:23:35 -0700

    Surprise typos

--- webpack.config.js
+++ webpack.config.js
@@ -47,23 +47,29 @@ module.exports = (env) => {
     plugins: [
       CSSExtract,
       new webpack.DefinePlugin({
-        'process.env.FIREBASE_API_KEY': JSON.stringify(FIREBASE_API_KEY),
+        'process.env.FIREBASE_API_KEY': JSON.stringify(
+          process.env.FIREBASE_API_KEY
+        ),
         'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
-          FIREBASE_AUTH_DOMAIN
+          process.env.FIREBASE_AUTH_DOMAIN
         ),
         'process.env.FIREBASE_DATABASE_URL': JSON.stringify(
-          FIREBASE_DATABASE_URL
+          process.env.FIREBASE_DATABASE_URL
         ),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(FIREBASE_PROJECT_ID),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
+          process.env.FIREBASE_PROJECT_ID
+        ),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
-          FIREBASE_STORAGE_BUCKET
+          process.env.FIREBASE_STORAGE_BUCKET
         ),
         'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
-          FIREBASE_MESSAGING_SENDER_ID
+          process.env.FIREBASE_MESSAGING_SENDER_ID
         ),
-        'process.env.FIREBASE_APP_ID': JSON.stringify(FIREBASE_APP_ID),
+        'process.env.FIREBASE_APP_ID': JSON.stringify(
+          process.env.FIREBASE_APP_ID
+        ),
         'process.env.FIREBASE_MEASUREMENT_ID': JSON.stringify(
-          FIREBASE_MEASUREMENT_ID
+          process.env.FIREBASE_MEASUREMENT_ID
         )
       })
     ],
